f42c82f3f0a195333d204fea81b2efe763c0a335
Yash <yashihathi@gmail.com>
2026-07-31 10:24:49 -0500

    updated clustering

--- Clustering.py
+++ Clustering.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.cluster import HDBSCAN
 
 def cluster(embeddings, df):
-    cluster_model = HDBSCAN(min_cluster_size = 20, metric = 'cosine')
+    cluster_model = HDBSCAN(min_cluster_size = 10, metric = 'cosine')
     clusters = cluster_model.fit_predict(embeddings)
     df["cluster_id"] = clusters
     df = df[df["cluster_id"] != -1]
